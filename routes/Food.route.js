46060ec43d2de440be08602a9747caf6eec81636
HariHaran9209 <90740755+HariHaran9209@users.noreply.github.com>
2025-05-18 20:50:35 +0530

    Update Food.route.js

--- routes/Food.route.js
+++ routes/Food.route.js
@@ -1,17 +1,29 @@
 import express from 'express'
 import { addFood, listFood, removeFood } from '../controller/Food.controller.js'
-import multer from 'multer'
+import { v2 as cloudinary } from 'cloudinary';
+import multer from 'multer';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const foodRouter = express.Router()
 
-const storage = multer.diskStorage({
-    destination: 'uploads',
-    filename: (req, file, cb) => {
-        return cb(null, `${Date.now()}${file.originalname}`)
-    }
-})
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-const upload = multer({ storage: storage })
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'your-folder-name', // optional folder in Cloudinary
+    allowed_formats: ['jpg', 'png', 'jpeg'],
+  },
+});
+
+const upload = multer({ storage: storage });
 
 foodRouter.post('/add', upload.single('image'), addFood)
 foodRouter.get('/list', listFood)
